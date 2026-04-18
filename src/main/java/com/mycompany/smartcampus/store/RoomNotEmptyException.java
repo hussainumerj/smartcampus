cf9b9f7585b12dea5d48fa85e0ae1252c8cc6385
Hussain <humerji12@gmail.com>
2026-04-18 02:37:38 +0100

    Fix package structure

--- src/main/java/com/mycompany/smartcampus/store/RoomNotEmptyException.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.smartcampus.store;
-
-public class RoomNotEmptyException extends RuntimeException {
-    public RoomNotEmptyException(String msg) { super(msg); }
-}
