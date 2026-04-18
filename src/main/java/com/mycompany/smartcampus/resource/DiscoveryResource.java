2cf2d3e78ee82f93d31323e840050c024f96a31f
Hussain <humerji12@gmail.com>
2026-04-19 00:39:35 +0100

    commit package

---
+++ src/main/java/com/mycompany/smartcampus/resource/DiscoveryResource.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampus.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
+
+@Path("/")
+public class DiscoveryResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> discovery() {
+        return Map.of(
+                "version", "v1",
+                "adminContact", "your.email@westminster.ac.uk",
+                "resources", Map.of(
+                        "rooms", "/api/v1/rooms",
+                        "sensors", "/api/v1/sensors"
+                )
+        );
+    }
+}
